eedbea0042622fc17b4792fb60577e9b51c84f7a
Paul-Emmanuel Pignol <ppignol@info-gnobotx.iut.bx1>
2013-02-19 10:41:34 +0100

    salut

--- test.c
+++ test.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <time.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 
 
 void afficherAide()
@@ -24,7 +29,22 @@ void afficherAide()
 }
 
 
-     
+
+int nombreLignes (FILE* f) {
+
+	char ligne[90];
+	int nb_ligne;
+	while (!feof(f))
+	{	fgets(ligne, 90, f);
+		++nb_ligne;
+	}
+	fclose(f);
+	return nb_ligne;
+}
+
+ 
+
+   
 int main (int argc, char **argv)
      {
 	   int tflag = 0; int xflag = 0; int dflag = 0;
@@ -35,7 +55,8 @@ int main (int argc, char **argv)
        int c;
      
        opterr = 0;
-     
+       
+// Pour gérer les options     
        if ((c = getopt (argc, argv, "hvctruxfzdm:sparse:")) != -1)
        {
 		   int nb=1;
@@ -79,38 +100,140 @@ int main (int argc, char **argv)
             exit(EXIT_FAILURE);
            }          				        
      }
+     
+//-c : pour créer une archive à partir d'une liste de fichier
 	 if (cflag == 1)
 	 {
 		 int nb=2;
+		 int nb_ligne = 0;
 		 FILE *f_in;
+		 time_t date;
 		 if ((f_in = fopen(argv[argc-1],"w")) == NULL)
 		{
 			fprintf(stderr, "\nErreur: Impossible de lire le fichier %s\n",argv[argc-1]);
 		}
-		 while (nb<argc && argc>1 )
+		 while (nb<argc-1 && argc>1 )
 		 {
 			 FILE *f_out;
-			 if ((f_out = fopen(argv[nb],"r")) == NULL)
+			if ((f_out = fopen(argv[nb],"r")) == NULL)
 			{
 				fprintf(stderr, "\nErreur: Impossible de lire le fichier %s\n",argv[nb]);
 			}
+			nb_ligne = nombreLignes(f_out);
 			char ligne[90];
-
-fgets(ligne, 90, f_out);
- 
-				while (!feof(f_out))
-				{
-					fputs(ligne, f_in);
-					fgets(ligne, 81, f_out);
-				}
-
-
+			fgets(ligne, 90, f_out);
+			fputs(argv[nb],f_in);
+			fputc('\n',f_in);
+			char nbLigne [10];
+			snprintf(nbLigne, 10, "%i", nb_ligne);
+			fputs(nbLigne,f_in);
+			fputc('\n',f_in);
+			date = time(NULL);
+			fputs( ctime(&date),f_in);
+			fputc('\n',f_in);
+			if ((f_out = fopen(argv[nb],"r")) == NULL)
+			{
+				fprintf(stderr, "\nErreur: Impossible de lire le fichier %s\n",argv[nb]);
+			}
+			while (!feof(f_out))
+			{
+				fputs(ligne, f_in);
+				fgets(ligne, 90, f_out);
+			}
+			fputc('\n',f_in);
 			fclose(f_out);
 			nb++;
 		 }
 		fclose(f_in);		
 	 }
 	 
+// -r : pour ajouter de nouveaux fichiers
+	 if (rflag==1)
+	 {
+		 int nb=2;
+		 int nb_ligne = 0;
+		 FILE *f_in;
+		 time_t date;
+		 if ((f_in = fopen(argv[argc-1],"a")) == NULL)
+		{
+			fprintf(stderr, "\nErreur: Impossible de lire le fichier %s\n",argv[argc-1]);
+		}
+		 while (nb<argc-1 && argc>1 )
+		 {
+			 FILE *f_out;
+			if ((f_out = fopen(argv[nb],"r")) == NULL)
+			{
+				fprintf(stderr, "\nErreur: Impossible de lire le fichier %s\n",argv[nb]);
+			}
+			nb_ligne = nombreLignes(f_out);
+			char ligne[90];
+			fgets(ligne, 90, f_out);
+			fputs(argv[nb],f_in);
+			fputc('\n',f_in);
+			char nbLigne [10];
+			snprintf(nbLigne, 10, "%i", nb_ligne);
+			fputs(nbLigne,f_in);
+			fputc('\n',f_in);
+			date = time(NULL);
+			fputs( ctime(&date),f_in);
+			fputc('\n',f_in);
+			if ((f_out = fopen(argv[nb],"r")) == NULL)
+			{
+				fprintf(stderr, "\nErreur: Impossible de lire le fichier %s\n",argv[nb]);
+			}
+			while (!feof(f_out))
+			{
+				fputs(ligne, f_in);
+				fgets(ligne, 90, f_out);
+			}
+			fputc('\n',f_in);
+			fclose(f_out);
+			nb++;
+		 }
+		fclose(f_in);	
+	 }
+	 
+// -u : pour mettre à jour l'archive si les fichiers lités sont plus récents que ceux archivés	 
+	 if (uflag==1)
+	 {	
+		 FILE *f_in1;
+		 int nb=2;
+		 if ((f_in1 = fopen(argv[argc-1],"a")) == NULL)
+		{
+			fprintf(stderr, "\nErreur: Impossible de lire le fichier %s\n",argv[argc-1]);
+		}
+		while (EOF != (c = fgetc(f_in1)))
+		{
+			char* nbL;
+			int ligne = 0;
+			int ma_ligne = 2;
+			int taille;
+			char* dateIn;
+			char* dateOut;
+
+			if ('\n' == c)
+            {
+				if(ligne == 1)
+					//nbL = fgets(ligne, 90, f_in1);
+                if (ligne == ma_ligne)
+                   break;
+                ligne++;
+            }
+        
+
+        if (ligne == ma_ligne)
+        {
+         //dateIn = fgets(ligne, 90, f_in1);
+        }
+		
+			while (nb<argc-1 && argc>1 )
+			{ 	FILE *f_out;
+			fclose(f_out);
+			nb++;
+			}
+		}
+		fclose(f_in1);		
+	 } 
      return 0;
 }
 
